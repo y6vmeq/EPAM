1301affdc7214127cd272e093194dbbe2102b27f
Aliaksandr_Padrabinkin <y6vmeq@gmail.com>
2019-03-24 00:23:50 +0300

    add test

--- Module9/module9/test/integration/default/default_test.rb
+++ Module9/module9/test/integration/default/default_test.rb
@@ -1,18 +1,14 @@
-# # encoding: utf-8
-
-# Inspec test for recipe module9::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe service('docker') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file("/config.yml") do
+  it { should exist } 
+end
+
+describe docker_container('registry') do
+  it { should exist }
+  it { should be_running }
 end
